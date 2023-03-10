bf40cdb846ef1722d9a100517d23a2b38cd5113f
Mosengoyo <123177406+Mosengoyo@users.noreply.github.com>
2023-03-10 11:38:57 -0500

    Add files via upload
    quickhired tuto django complété

--- QuickHired/members/models.py
+++ QuickHired/members/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Member(models.Model):
-	firstname = models.CharField(max_length=30)
-	lastname = models.CharField(max_length=30)
+	firstname = models.CharField(max_length=100)
+	lastname = models.CharField(max_length=100)
 	phone = models.IntegerField(null=True)
 	joined_date = models.DateField(null=True)
 	note = models.FloatField(null=True)
+
+def __str__(self):
+	return f"{self.firstname} {self.lastname}"
