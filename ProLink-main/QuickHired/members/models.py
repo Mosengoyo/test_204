a67727727d6d67d56951d887bc23d0795b39fd5e
Mosengoyo <123177406+Mosengoyo@users.noreply.github.com>
2023-04-14 00:31:10 -0400

    Avec Panier (vues temporaires)

---
+++ ProLink-main/QuickHired/members/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Member(models.Model):
+	firstname = models.CharField(max_length=100)
+	lastname = models.CharField(max_length=100)
+	phone = models.IntegerField(null=True)
+	joined_date = models.DateField(null=True)
+	note = models.FloatField(null=True)
+	def __str__(self):
+		return f"{self.firstname} {self.lastname}"
+
+class Product(models.Model):
+	name = models.TextField()
+	description = models.TextField()
+	price = models.DecimalField(max_digits=6, decimal_places=2)
+	image = models.ImageField(upload_to='products/')
+	def __str__(self):
+		return f"{self.name} {self.description}"
+
+
+
+
+class Cart(models.Model):
+	user = models.TextField()
+	product = models.TextField() # models.ForeignKey(Product, on_delete=models.CASCADE)
+	quantity = models.IntegerField()
+	total_price = models.DecimalField(max_digits=6, decimal_places=2)
+	def __str__(self):
+		return f"{self.user} {self.product}"
+
---
+++ ProLink-main/QuickHired/members/views.py
@@ -0,0 +1,40 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+	mymembers = Member.objects.all().values()
+	template = loader.get_template('all_members.html')
+	context = {
+		'mymembers' : mymembers,
+	}
+	return HttpResponse(template.render(context, request))
+
+def details(request, id):
+	mymember = Member.objects.get(id=id)
+	template = loader.get_template('details.html')
+	context = {
+		'mymember' : mymember,
+	}
+	return HttpResponse(template.render(context, request))
+
+def main(request):
+	template = loader.get_template('PageAccueil/PageAc.html')
+	return HttpResponse(template.render())
+
+def panier(request):
+	context = {}
+	return render(request, 'PagePanier/Panier.html', context)
+
+def checkout(request):
+	context = {}
+	return render(request, 'main/Checkout.html')
+
+def testing(request):
+	template = loader.get_template('template.html')
+	context = {
+		'fruits': ['Apple', 'Banana', 'Cherry'],
+	}
+	return HttpResponse(template.render(context, request))
